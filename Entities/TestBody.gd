6eedc34a1edb1cc733df093577df08891152d930
Michael Reel <mykilr@gmail.com>
2021-10-24 12:05:35 +0100

    Checking the collisions work on the base layer

---
+++ Entities/TestBody.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+
+var velocity := Vector3.ZERO
+var gravity := 100
+
+
+func _physics_process(delta : float):
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y -= gravity * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
