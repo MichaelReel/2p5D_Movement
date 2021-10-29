81ba1d65e495caf33fa19abad96f444a15958a0c
Michael Reel <mykilr@gmail.com>
2021-10-29 22:12:11 +0100

    New scene to test baked navigation with bat

--- Entities/Bat.gd
+++ Entities/Bat.gd
@@ -3,21 +3,34 @@ extends KinematicBody
 
 export (int) var current_HP : int = 10
 export (int) var max_HP : int = 10
-
-export (float) var move_speed : float = 5.0
+export (float) var move_speed : float = 500.0
 export (float) var gravity : float = 9.8
 
 
+onready var nav : Navigation = get_parent() 
+onready var player := $"../../Player"   # Need to get with collision areas
+
+
 var vel := Vector3.ZERO
+var path := []
+var path_node := 0
 
 
 func _physics_process(delta : float):
-	# Reset velocity except jump/gravity (necessary?)
-	vel.x = 0.0
-	vel.z = 0.0
-	
-	# Gravity!
-	vel.y -= gravity * delta
+	if path_node < path.size():
+		var direction = path[path_node] - global_transform.origin
+		# direction.y -= gravity * delta
+		
+		if direction.length() < 1:
+			path_node += 1
+		else:
+			vel = move_and_slide(direction.normalized() * move_speed * delta, Vector3.UP)
 
-	# Move the player along the current velocity
-	vel = move_and_slide(vel, Vector3.UP)
+
+func move_to(target_pos : Vector3):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
+	path_node = 0
+
+
+func _on_Timer_timeout():
+	move_to(player.global_transform.origin)
