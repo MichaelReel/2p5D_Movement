0de7c5063f680075ad8dac30766c664d1028d4e9
Michael Reel <mykilr@gmail.com>
2021-10-31 17:48:25 +0000

    Some modifications to the player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,47 +1,74 @@
 extends KinematicBody
 
 
-export (int) var current_HP : int = 10
-export (int) var max_HP : int = 10
-export (int) var damage : int = 1
-export (int) var gold : int = 0
-export (float) var attach_rate : float = 0.3
-export (float) var move_speed : float = 5.0
-export (float) var jump_force : float = 10.0
-export (float) var gravity : float = 25
-
-var last_attack_time : int = 0
-var vel := Vector3.ZERO
-
-onready var camera := $CameraOrbit
-onready var attack_ray_cast := $AttackRayCast
+enum {
+	MOVE,
+	ATTACK
+}
 
 
-func _physics_process(delta : float):
-	# Reset velocity except jump/gravity (necessary?)
-	vel.x = 0.0
-	vel.z = 0.0
-	
-	# Get the input mouse vector
-	var input = Vector3(
-		Input.get_action_strength("move_left") - Input.get_action_strength("move_right"),
-		0,
-		Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
-	).normalized() 
-	
-	# Get the look direction, modified by the input vector 
-	# BTW, much of this relies on the camera and player looking in the +z direction
-	var dir : Vector3 = (transform.basis.z * input.z + transform.basis.x * input.x)
-	vel.x = dir.x * move_speed
-	vel.z = dir.z * move_speed
-	
+export (float) var acceleration := 150.0
+export (float) var air_friction := 4.0
+export (float) var ground_friction := 80.0
+export (float) var max_speed := 5.0
+export (float) var jump_force := 10.0
+export (float) var gravity := 25.0
+export (bool) var air_control := true
+
+
+var state : int = MOVE
+var velocity := Vector3.ZERO
+var horizontal_velocity := Vector2.ZERO
+
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			state = MOVE
+
+
+func move_state(delta : float):
+	var input_vector := _normalized_input_vector()
+	if input_vector != Vector2.ZERO and _movement_allowed():
+		# Speed up - Apply player WSAD movement as horizontal acceleration
+		var basis_z := Vector2(transform.basis.z.x, transform.basis.z.z)
+		var basis_x := Vector2(transform.basis.x.x, transform.basis.x.z)
+		var horizonal_dir := basis_z * input_vector.y + basis_x * input_vector.x
+		horizontal_velocity = horizontal_velocity.move_toward(
+			horizonal_dir * max_speed, acceleration * delta
+		)
+	else:
+		# Slow down - apply horizontal friction
+		var friction = air_friction
+		if is_on_floor():
+			friction = ground_friction
+		horizontal_velocity = horizontal_velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	velocity.x = horizontal_velocity.x
+	velocity.z = horizontal_velocity.y
+		
 	# Gravity!
 	if not is_on_floor():
-		vel.y -= gravity * delta
-	
+		velocity += Vector3.DOWN * gravity * delta
+
 	# Jump input
 	if Input.is_action_pressed("move_jump") and is_on_floor():
-		vel.y = jump_force
+		velocity += Vector3.UP * jump_force
+		
+	velocity = move_and_slide(velocity, Vector3.UP)
 	
-	# Move the player along the current velocity
-	vel = move_and_slide(vel, Vector3.UP)
+	if Input.is_action_just_pressed("move_attack"):
+		state = ATTACK
+
+
+func _movement_allowed() -> bool:
+	return air_control or is_on_floor()
+
+
+func _normalized_input_vector() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_left") - Input.get_action_strength("move_right"),
+		Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+	).normalized()
