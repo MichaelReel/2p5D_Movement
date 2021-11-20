302217e5b0d59c009ba97d44c6bb7a8659e24fa8
Michael Reel <mykilr@gmail.com>
2021-11-20 17:01:54 +0000

    Added a mesh based particle on weapon collect

--- Player/Weapons/WeaponPickup.gd
+++ Player/Weapons/WeaponPickup.gd
@@ -1,14 +1,20 @@
 extends Spatial
 
-const PickedUpEffect := preload("res://Effects/DeathEffect.tscn")
+const PickedUpEffect := preload("res://Effects/CollectEffect.tscn")
 
 export (PackedScene) var weapon
 
+onready var mesh_instance := $MeshInstance
 onready var parent := get_parent()  # For death effect + debug, could be scene root
+
+var picked_up_effect
+
+func _ready():
+	picked_up_effect = PickedUpEffect.instance()
+	picked_up_effect.draw_pass_1 = mesh_instance.mesh
 
 
 func _create_picked_up_effect():
-	var picked_up_effect := PickedUpEffect.instance()
 	parent.add_child(picked_up_effect)
 	picked_up_effect.global_transform.origin = global_transform.origin
 	picked_up_effect.emitting = true
